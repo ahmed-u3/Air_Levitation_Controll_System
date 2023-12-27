917ab47762f539303ada3e01434badaeee988da0
Fares Hazem <fares.hzm12@gmail.com>
2023-12-28 01:34:18 +0200

    Update Air_Leviation_Control_System.ino

--- Air_Leviation_Control_System/Air_Leviation_Control_System.ino
+++ Air_Leviation_Control_System/Air_Leviation_Control_System.ino
@@ -1,13 +1,15 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 #include <PID_v1.h>
 
 // PID constants, set from MATLAB PID tuner
-#define kp 0.36962
-#define ki 0.50657
-#define kd 0.067425
+#define kp 0.897
+#define ki 0.1
+#define kd 0.01
 
 // Variables for PID control
 double Input;       // Current value (feedback)
-double output;      // Output value (control signal to motor)
+double output = 200; // Output value (control signal to motor)
 double SetPoint;    // Desired setpoint
 
 // Create PID controller object
@@ -22,9 +24,17 @@ float duration, distance;
 #define iN4 7
 int ENA = 10;
 
+// Initialize the LCD with its I2C address, number of columns, and number of rows
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 void setup() {
   // Initialize serial communication
   Serial.begin(9600);
+
+  // Initialize the LCD
+  lcd.begin();
+  lcd.backlight();
+  lcd.clear();
 
   // Set pin modes
   pinMode(iN3, OUTPUT);
@@ -38,28 +48,20 @@ void setup() {
 
   // Configure PID controller
   myPID.SetMode(AUTOMATIC);
-  myPID.SetOutputLimits(0, 255);
+  myPID.SetOutputLimits(50, 255);
 }
 
 void Ultrasonic() {
   // Trigger ultrasonic sensor
-  digitalWrite(trigPin, 1);
+  digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, 0);
+  digitalWrite(trigPin, LOW);
 
   // Measure echo duration
-  duration = pulseIn(echoPin, 1);
+  duration = pulseIn(echoPin, HIGH);
 
   // Calculate distance from duration
   distance = 0.017 * duration;
-
-  // Print distance to serial monitor
-  Serial.print(distance);
-  Serial.write(13);
-  Serial.write(10);
-
-  // Delay to avoid rapid serial prints
-  delay(500);
 }
 
 void loop() {
@@ -79,8 +81,8 @@ void loop() {
   analogWrite(ENA, output);
 
   // Motor control logic
-  digitalWrite(iN3, 1);
-  digitalWrite(iN4, 0);
+  digitalWrite(iN3, HIGH);
+  digitalWrite(iN4, LOW);
 
   // Print relevant information to the serial monitor
   Serial.print("Distance: ");
@@ -89,12 +91,21 @@ void loop() {
   Serial.print("Setpoint: ");
   Serial.print(SetPoint);
   Serial.print(" cm\t");
-  Serial.print("Output: ");
-  Serial.print(output);
-  Serial.print("\tSpeed: ");
-  Serial.println(output);  // Assuming 255 is full speed
+  Serial.print("Speed: ");
+  Serial.print(output); // Assuming 255 is full speed
+  Serial.println(" PWM");
 
-  // Delay for better readability in the serial monitor
-  delay(500);
+  // Print information to the LCD
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("D: ");
+  lcd.print(distance);
+  lcd.setCursor(10, 0);
+  lcd.print("SP: ");
+  lcd.print(SetPoint);
+  lcd.setCursor(0, 1);
+  lcd.print("S: ");
+  lcd.print(output);
+
+  delay(100);
 }
-
